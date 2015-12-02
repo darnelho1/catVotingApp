d43d298ea196bb8c88bd2acbbcf428d6177a43a9
Darnell <darnelho1@gmail.com>
2015-12-01 23:08:47 -0800

    move script.js JavaScript/script.js add cat images in images/ add tracer object

---
+++ JavaScript/script.js
@@ -0,0 +1,23 @@
+var cats =[];
+
+var CatMaker = function(name, fileLocation,votes,viewed,catColor,hasStripes){
+  this.name = name;
+  this.fileLocation = fileLocation;
+  this.votes = 0;
+  this.viewed = false;
+  //this.catColor = catColor;
+  this.hasStripes =
+    cats.push(this);
+
+}
+
+var tracker = {
+    totalSelections = 0;
+    picPref1 = 0;
+    picPref2 = 0;
+    prefStripes = 0;
+
+    ]
+}
+
+
